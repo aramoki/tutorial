9a6bb9d51bed59e353af5960cb5cf2bde9fd3f71
altugyigit <altug_yigit@hotmail.com>
2017-10-29 16:09:28 +0200

    Sub numbers

--- ornek.cpp
+++ ornek.cpp
@@ -3,7 +3,12 @@
 
 using namespace std;
 
+int subnumbers(int a,int b)
+{
+ return (a-b)
+}
+
 int main() {
-	printf("Hello world. Biseyler Birseyler2\n");
+	printf("3 - 2 = %i",subnumbers(3,2));
 	return 0;
 }
